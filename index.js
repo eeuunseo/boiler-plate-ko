a23506b9ab903e345bd7a0ef26e7037ba329b055
eeuunseo <eeuunseo@gmail.com>
2023-05-08 16:00:09 +0900

    pm4:00

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 4000;
 const config = require("./config/key");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const {User} = require("./models/User");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose
@@ -26,10 +28,38 @@ app.post('/register', (req, res) => {
   // 그것들을 데이터 베이스에 넣어준다.
   const user = new User(req.body);
   user.save((err, userInfo) => {
-    if(err) return res.json({success: false, err})
+    if(err) return res.json({success: false, err});
     return res.status(200).json({
       success: true
     })
   })
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일이 DB 내에 있는지 확인
+  User.findOne({
+    email: req.body.email
+  }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 요청된 이메일이 DB 내에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+      
+      // 비밀번호 까지 맞다면 토큰 생성
+      user.generateToken((err, user) => {
+          if(err) return res.status(400).send(err);
+
+          // 토큰을 저장(보관), 쿠키/세션/로컬 스토리지
+          res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id })
+      })
+    })
+  })
+  
 })
 app.listen(port, () => console.log('Example app!'));
